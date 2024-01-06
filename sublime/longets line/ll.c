9a54e0f306d1081381bdddad570885c82af197d2
Niko <podzemgaz@gmail.com>
2024-01-06 21:28:04 +0300

    Changed program to the working state

--- sublime/longets line/ll.c
+++ sublime/longets line/ll.c
@@ -1,31 +1,25 @@
 #include "stdio.h"
 
-#define MAXLINE 1000 
+#define MAXLINE 1000 /* максимальный размер вводимой строки */
 
 int getline1(char s[], int lim);
 void copy(char to[], char fromf[]);
 
 /* печать самой длинной строки */
-int main(int argc, char const *argv[])
+int main()
 {
 	int len; /* длина текущей строки */
  	int max; /* длина максимальной из просмотренных строк */
  	char line[MAXLINE]; /* текущая строка */
  	char longest[MAXLINE]; /* самая длинная строка */
  	max = 0;
- 	// while ((len = getline1(line, MAXLINE)) > 0)
- 	// 	if (len > max) {
- 	// 		max = len;
- 	// 		copy(longest, line);
- 	// 	}
- 		//if (max > 0) /* была ли хоть одна строка? */
- 		//printf("%s", longest);
-
- 	len = getline1(line, MAXLINE);
-
- 	printf("%d\n", len);
-
- 		return 0;
+ 	while ((len = getline1(line, MAXLINE)) > 0)
+ 		if (len > max) {
+ 	 		max = len;
+ 	 		copy(longest, line);
+ 	 	}
+ 		if (max > 0) /* была ли хоть одна строка? */
+ 			printf("%s", longest);
 }
 	
 /* getline: читает строку в s; возвращает длину */
